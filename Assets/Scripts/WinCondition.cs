bf42ba44b71c24aa6a5e530d74dc68d75f701960
Esma-MickaelD <m.dacostareis@rns.ecolescreatives.com>
2025-05-23 15:38:52 +0200

    Nothing

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -15,8 +15,6 @@ public class WinCondition : MonoBehaviour
     
     [SerializeField] private PropMovement _propToMove;
     public bool PropHasMoved = false;
-
-    [SerializeField] private int _propToFxIndex = 0;
    
     private void Update()
     {
